ec596dc2ad35cc3ecf603022c1533bb073696f93
tugceydgn <tgcaydogan@hotmail.com>
2020-04-22 17:04:14 -0500

    latest version

--- src/test/java/com/automation/Tests/Day03/FindElementsPractice.java
+++ src/test/java/com/automation/Tests/Day03/FindElementsPractice.java
@@ -2,19 +2,21 @@ package com.automation.Tests.Day03;
 
 import com.automation.Utilities.DriverFactory;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 
 public class FindElementsPractice {
 
     public static void main(String[] args) throws Exception {
 
-        //    WebDriverManager.chromedriver().setup();
-        //  WebDriver driver = new ChromeDriver();
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
 
-        WebDriver driver = DriverFactory.createADriver("chrome");
+        //   WebDriver driver = DriverFactory.createADriver("chrome");
 
         driver.get("http://practice.cybertekschool.com/sign_up");
         WebElement fullName = driver.findElement(By.name("full_name"));
@@ -31,8 +33,13 @@ public class FindElementsPractice {
         signUpButton.submit();
         Thread.sleep(2000);
 
+        String expected = "Thank you for signing up. Click the button below to return to the home page.";
         WebElement signUpMessage = driver.findElement(By.name("signup_message"));
-        System.out.println(signUpMessage.getText());
+        if (expected.equals(signUpMessage.getText())) {  //To get the text <h3>text</h3>
+            System.out.println("TEST PASSED");
+        } else {
+            System.out.println("TEST FAILED");
+        }
 
 
         driver.quit(); //to close everything
